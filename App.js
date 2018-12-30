4f4180d258eb53933c265dbb2016d55435044e16
Abdelrahman-Labib <abdolabib602@yahoo.com>
2018-12-31 00:47:40 +0200

    update

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import RootNavigation from './src/navigation/RootNavigation';
+
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <RootNavigation/>
+    );
+  }
+}
+
---
+++ src/constants/config.js
@@ -0,0 +1,6 @@
+
+const SERVER_URL = 'http://192.168.138.1:80/';
+
+export default {
+    url:SERVER_URL,
+};
